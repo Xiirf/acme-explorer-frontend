243e66dcfb2d21d9f8653c702fbc9b36ad17b4d4
qenjalbert <quentin.enjalbert@insa-rouen.fr>
2020-03-16 21:58:34 +0100

    Add Trip service

---
+++ src/app/services/trip.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Trip } from '../models/trip.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+
+  constructor(private http: HttpClient) { }
+
+  getTrips(): Trip[] {
+    return this.http.get<Trip[]>("http://localhost:8080/v1/trips")
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
+}
