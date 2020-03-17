a510f415ffb99a43334094fe3057bcc4b387fbab
qenjalbert <quentin.enjalbert@insa-rouen.fr>
2020-03-17 23:11:14 +0100

    Add auth service with register

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+
+import { Actor } from '../models/actor.model';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private fireAuth: AngularFireAuth, private http: HttpClient, public router: Router) { }
+
+  register(actor: Actor): Promise<Actor> {
+    return this.http.post<Actor>(`${environment.backendApiBaseUrl}/actors`, actor, httpOptions).toPromise();
+  }
+}
